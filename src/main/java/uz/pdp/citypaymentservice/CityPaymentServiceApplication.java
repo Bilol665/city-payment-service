60d54f4ce76d959688733292bc24f66b5517f6f4
Bilol665 <bilolsultan665@gmail.com>
2023-08-02 16:54:31 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/citypaymentservice/CityPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.citypaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/citypaymentservice/CityPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.citypaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
