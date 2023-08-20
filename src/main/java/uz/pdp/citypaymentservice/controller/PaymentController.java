2cf197af326a2494b440337f9ef0678f7606d04b
Bilol665 <bilolsultan665@gmail.com>
2023-08-21 03:44:21 +0500

    To Fazliddin!

--- src/main/java/uz/pdp/citypaymentservice/controller/PaymentController.java
+++ src/main/java/uz/pdp/citypaymentservice/controller/PaymentController.java
@@ -1,16 +1,17 @@
 package uz.pdp.citypaymentservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.citypaymentservice.domain.dto.CardDto;
 import uz.pdp.citypaymentservice.domain.dto.P2PDto;
 import uz.pdp.citypaymentservice.domain.entity.card.CardEntity;
 import uz.pdp.citypaymentservice.service.card.CardService;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,12 +27,55 @@ public class PaymentController {
     ){
         return  ResponseEntity.ok(cardService.peerToPeer(p2PDto,principal));
     }
-//    @PutMapping("/transact")
-//    public ResponseEntity<CardEntity>p2p(
-//            Principal principal,
-//            @RequestParam UUID receiver,
-//            @RequestParam Double balance
-//    ){
-//        return  ResponseEntity.ok(cardService.peerToPeer(principal,receiver,balance));
-//    }
+    @PostMapping("/card/save")
+    public ResponseEntity<CardEntity> save(
+            Principal principal,
+            @RequestBody CardDto cardDto
+    ){
+        return ResponseEntity.ok(cardService.saveCard(cardDto,principal));
+    }
+    @GetMapping("/card/get-b-user/{card}")
+    public ResponseEntity<UUID> get(
+            @PathVariable String card
+    ) {
+        return ResponseEntity.ok(cardService.getUserByCard(card));
+    }
+    @GetMapping("/card/get/{cardId}")
+    public ResponseEntity<String> getById(
+            @PathVariable UUID cardId
+    ) {
+        return ResponseEntity.ok(cardService.getById(cardId));
+    }
+    @GetMapping("/card/get")
+    public ResponseEntity<List<CardEntity>>get(
+            Principal principal
+    ){
+        return ResponseEntity.ok(cardService.getCard(principal));
+    }
+
+    @PutMapping("/card/update/{id}")
+    public ResponseEntity<CardEntity>update(
+            @PathVariable UUID id,
+            @RequestBody CardDto cardDto
+    ){
+        return ResponseEntity.ok(cardService.updateCardById(id,cardDto));
+    }
+
+
+    @DeleteMapping("/card/delete/{id}")
+    public ResponseEntity<HttpStatus>delete(
+            @PathVariable UUID id
+    ){
+        cardService.deleteCardById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/card/fill/{id}")
+    public ResponseEntity<CardEntity>fill(
+            Principal principal,
+            @PathVariable UUID id,
+            @RequestParam Double balance
+    ){
+        return ResponseEntity.ok(cardService.fillBalance(id,balance,principal));
+    }
 }
